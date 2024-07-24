9050fdc79331a7cec8590f77958f7323d6ec3581
LytheanSem <lytheansem430@gmail.com>
2024-07-24 13:24:08 +0700

    update vite

--- React/accessory-shop/src/App.jsx
+++ React/accessory-shop/src/App.jsx
@@ -5,11 +5,8 @@ import Button from "react-bootstrap/Button";
 import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  // For a component, it must return a single JSX element.
-  // So if you have multiple elements, you need to wrap them in a single element.
-  // You can use <>...</> to wrap multiple elements (only in JSX).
-
   const [selectedItems, setSelectedItems] = useState([]);
+  const [filteredSelectedItems, setFilteredSelectedItems] = useState([]);
 
   const quantityRef = useRef();
   const productRef = useRef();
@@ -21,27 +18,44 @@ function App() {
       (accessory) => accessory.id === productId
     );
     const order = {
-      // id: product.id,
-      // name: product.name,
-      // price: product.price,
       ...product,
       quantity: quantityRef.current.value,
     };
-    console.table(order);
-    selectedItems.push(order); // this does not update DataTable, why?
-    // setSelectedItems(selectedItems) // not working, it is the same reference to the array.
-    setSelectedItems([...selectedItems]); // this works, it creates new array
+    selectedItems.push(order);
+    setSelectedItems([...selectedItems]);
+    setFilteredSelectedItems([...selectedItems]);
+  };
+
+  const deleteItemByIndex = (index) => {
+    selectedItems.splice(index, 1);
+    setSelectedItems([...selectedItems]);
+    setFilteredSelectedItems([...selectedItems]);
+  }
+
+  const filter = (keyword) => {
+    const filteredItems = selectedItems.filter((item) => item.name.includes(keyword))
+    setFilteredSelectedItems(filteredItems)
+  }
+
+  const sortAscending = () => {
+    const sortedItems = [...selectedItems].sort((a, b) => a.name.localeCompare(b.name));
+    setSelectedItems(sortedItems);
+    setFilteredSelectedItems(sortedItems);
+  };
+
+  const sortDescending = () => {
+    const sortedItems = [...selectedItems].sort((a, b) => b.name.localeCompare(a.name));
+    setSelectedItems(sortedItems);
+    setFilteredSelectedItems(sortedItems);
   };
 
   const updatePrice = (e) => {
-    const productId = parseInt(e.target.value); // Integer expected
+    const productId = parseInt(e.target.value);
     const product = accessoryData.find(
       (accessory) => accessory.id === productId
     );
     setPrice(product.price);
   };
-
-  // console.table(accessoryData)
 
   return (
     <>
@@ -57,44 +71,29 @@ function App() {
               ))}
             </select>
           </Col>
-          <Col> Price: </Col>
-          <Col> {price} </Col>
-
-          <Col> Quantity: </Col>
-          <Col>
+        </Row>
+        <Row>
+          <Col xs={2}> Price: </Col>
+          <Col xs={10}> {price} </Col>
+        </Row>
+        <Row>
+          <Col xs={2}> Quantity: </Col>
+          <Col xs={10}>
             <input type="number" ref={quantityRef} defaultValue={1} />{" "}
           </Col>
         </Row>
+        <Button onClick={handleSubmit}>Submit</Button>
       </Container>
-      <Button onClick={handleSubmit}>Submit</Button>
 
       <Container>
-        <DataTable data={selectedItems} />
+        <DataTable
+          data={filteredSelectedItems}
+          onDelete={deleteItemByIndex}
+          onFilter={filter}
+          onSortAscending={sortAscending}
+          onSortDescending={sortDescending}
+        />
       </Container>
-      {/* <div className="container">
-        <div className="row">
-          <div className="col-2"> Product: </div>
-          <div className="col-10">
-            <select ref={productRef} onChange={updatePrice}>
-              {accessoryData.map((accessory, index) => (
-                <option key={index} value={accessory.id}>
-                  {accessory.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="col-2"> Price: </div>
-          <div className="col-10"> {price} </div>
-
-          <div className="col-2"> Quantity: </div>
-          <div className="col-10">
-            <input type="number" ref={quantityRef} defaultValue={1} />{" "}
-          </div>
-        </div>
-      </div>
-      <Button onClick={handleSubmit}>Submit</Button>
-
-      <DataTable data={selectedItems} />  */}
     </>
   );
 }
--- React/accessory-shop/src/components/DataTable.jsx
+++ React/accessory-shop/src/components/DataTable.jsx
@@ -1,28 +1,58 @@
 import React from "react";
+import { Container, Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 
-const DataTable = ({ data }) => {
+const DataTable = ({ data, onDelete, onFilter, onSortAscending, onSortDescending }) => {
+  const sRef = React.useRef()
+
+  const handleSearch = () => {
+    const keyword = sRef.current.value
+    console.debug("Search", keyword)
+    onFilter(keyword)
+  }
+
+  const handleDelete = (index) => {
+    console.debug("Delete", index)
+    onDelete(index)
+  }
+
   return (
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Qty</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.quantity}</td>
-            <td>{item.price}</td>
+    <Container>
+      <input type="text" placeholder="Search..." ref={sRef} />{' '}
+      <Button onClick={handleSearch} variant="outline-dark">
+        <i className="bi bi-search"></i> Search
+      </Button>{' '}
+      Sort by:
+      <Button onClick={onSortAscending} variant="outline-dark">
+        <i className="bi bi-arrow-up-circle-fill"></i>
+      </Button>{' '}
+      <Button onClick={onSortDescending} variant="outline-dark">
+        <i className="bi bi-arrow-down-circle-fill"></i>
+      </Button>
+
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Qty</th>
+            <th>Price</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>
+                <i className="bi bi-trash" onClick={() => handleDelete(index)}></i>
+              </td>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 
